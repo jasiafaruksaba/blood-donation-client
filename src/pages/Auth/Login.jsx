fead1e6d87cb8a9475830e3a5d432a3c3ac933cb
jasiafaruksaba <sabajasiafaruk@gmail.com>
2026-04-03 21:21:34 +0600

    login file

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { loginUser } = useAuth();
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      await loginUser(data.email, data.password);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+
+        <input {...register("email")} placeholder="Email" className="input" />
+
+        <input {...register("password")} type="password" placeholder="Password" className="input" />
+
+        <button className="btn btn-primary w-full">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
