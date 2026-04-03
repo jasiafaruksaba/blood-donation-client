b509d0a0e8ea3da5ce270e80ec69c444b6daff33
jasiafaruksaba <sabajasiafaruk@gmail.com>
2026-04-03 21:18:26 +0600

    register file

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
 
 const Register = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { registerUser } = useAuth();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      await registerUser(
+        data.email,
+        data.password,
+        data.name,
+        data.avatar
+      );
+      alert("Registered Successfully");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+
+        <input {...register("name")} placeholder="Name" className="input" />
+
+        <input {...register("email")} placeholder="Email" className="input" />
+
+        <input {...register("avatar")} placeholder="Image URL" className="input" />
+
+        <input {...register("password")} type="password" placeholder="Password" className="input" />
+
+        <button className="btn btn-primary w-full">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
