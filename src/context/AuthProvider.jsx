714aa4ae4964f6100514db8291e348958f834b4e
jasiafaruksaba <sabajasiafaruk@gmail.com>
2026-04-03 21:11:09 +0600

    updated authprovider file

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,31 +1,86 @@
 import { createContext, useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../firebase/firebase.init.js"; 
-import { signOut } from "firebase/auth";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+import app from "../../firebase/firebase.init.js";
+import axiosPublic from "../api/axiosPublic";
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const logout = () => {
-    setLoading(true);
-    return signOut(auth);
-}
 
+  // register
+  const registerUser = async (email, password, name, photo) => {
+    const result = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    await updateProfile(result.user, {
+      displayName: name,
+      photoURL: photo,
+    });
+
+    // save user in DB
+    const userInfo = {
+      name,
+      email,
+      avatar: photo,
+      role: "donor",
+      status: "active",
+    };
+
+    await axiosPublic.post("/users", userInfo);
+
+    return result;
+  };
+
+  // login
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // logout
+  const logoutUser = () => signOut(auth);
+
+  // observer
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
       setUser(currentUser);
+
+      if (currentUser?.email) {
+        // get JWT
+        const res = await axiosPublic.post("/jwt", {
+          email: currentUser.email,
+        });
+
+        localStorage.setItem("access-token", res.data.token);
+      } else {
+        localStorage.removeItem("access-token");
+      }
+
       setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
-  const authInfo = { 
-    user, 
+  const authInfo = {
+    user,
     loading,
-    logout 
+    registerUser,
+    loginUser,
+    logoutUser,
   };
 
   return (
