b82aabb70473711c1b8f257230c1d8587ec51328
jasiafaruksaba <sabajasiafaruk@gmail.com>
2026-04-03 21:07:48 +0600

    private route file

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import { Navigate } from "react-router";
+import useAuth from "../hooks/useAuth";
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (user) return children;
+
+  return <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
