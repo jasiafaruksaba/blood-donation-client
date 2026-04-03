6371ace56c536d4340ab712483e429eb3fd810d3
jasiafaruksaba <sabajasiafaruk@gmail.com>
2026-04-03 21:32:20 +0600

    admin route file

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import { Navigate } from "react-router";
+import useUserRole from "../hooks/useUserRole";
 
-const AdminRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AdminRoute = ({ children }) => {
+  const { role, loading } = useUserRole();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (role === "admin") return children;
+
+  return <Navigate to="/" />;
 };
 
 export default AdminRoute;
